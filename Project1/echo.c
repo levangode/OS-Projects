f7e23598ed8c43d66e94c3f40aabc71402aac575
levangode <lgode14@freeuni.edu.ge>
2016-10-02 00:27:11 +0400

    (echo) doesn't support ?$ yet
    *echo is also builtin*
    
    will be called similiar way as other builtIn functions, so takes half
    responsibility of parsing on itself

---
+++ Project1/echo.c
@@ -0,0 +1,37 @@
+#include "vector.h"
+#include "string.h"
+#include <stdlib.h>
+
+
+int lastChildPid=0;
+
+
+/* parses all commands and echoes as bash*/
+int do_echo(vector* commands){
+		int i;
+		int res = 0;
+		for(i=1; i<VectorLength(commands); i++){
+			char* secondArg = *(char**)VectorNth(commands, i);
+			if(secondArg[0] == '$'){
+				if(strlen(secondArg) > 1){
+					char* nextTok = secondArg+1;
+					if(*nextTok == '?'){
+						printf("%d ", lastChildPid);
+						if(strlen(nextTok) > 1){
+							printf("%s ", nextTok+1);
+						}
+					} else {
+						char* varName = nextTok;
+						char* varValue = getenv(varName);
+						if(varValue != NULL){
+							printf("%s ", varValue);
+						}
+					}
+				}
+			} else {
+				printf("%s ", *(char**)	VectorNth(commands, i));
+			}
+		}
+		printf("\n");
+		return res;
+}
