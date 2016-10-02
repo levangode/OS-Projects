a79da58ee9f6fa25d18ac25666ca3cf00fbf61f2
levangode <lgode14@freeuni.edu.ge>
2016-10-03 03:44:26 +0400

    (cd) tested, fixed

--- Project1/cd.c
+++ Project1/cd.c
@@ -9,28 +9,7 @@
 */
 int change_directory(char* dest){
 	char* path = dest;
-	bool was = false;
-	if(dest == NULL){
-		path = getenv("HOME");
-		if(path == NULL){
-			return -1;
-		}
-	} else if(*dest != '/'){
-		char buff[1024];
-		char* cur = getcwd(buff, 1024);
-		int len = strlen(cur);
-		path = malloc(len+strlen(path));
-		was=true;
-		strcpy(path, cur);
-		strcat(path, "/");
-		strcat(path, dest);
-	}
-
-	printf("%s\n", path);
 	int rVal = chdir(path);
-	if(was){
-		free(path);
-	}
 	if(rVal == -1){
 		perror("Cannot open directory");
 	}
