8a6d234e98acfabd008d093e64d5f68a03b7c42e
levangode <lgode14@freeuni.edu.ge>
2016-10-03 03:38:57 +0400

    (kill) tested, fixed

--- Project1/kill.c
+++ Project1/kill.c
@@ -1,22 +1,30 @@
 #include <sys/types.h>
 #include <signal.h>
-#include "helpers.c"
+
 
 /* Sends a signal to process passed by pid */
-int kill_it(char* pid, char* sig){
+int kill_it(char* sig, char* pid){
 	int p;
+	if(sig[0] == '-'){
+		sig++;
+	}
 	int s;
 	if(isnumber(pid)){
 		p=atoi(pid);
-	}  else if(isnumber(pid)){
+	} else {
+		printf("%s\n", "Wrong format of ID");
+		return -1;
+	}
+	if(isnumber(sig)){
 		s=atoi(sig);
 	} else {
 		printf("%s\n", "Wrong format of ID");
 		return -1;
 	}
+	printf("%d   %d\n",p,s);
 	int res = kill(p, s);
 	if(res != 0){
 		perror("Sending signal failed");
-		return res;
 	}
+	return res;
 }
