aceb4ec3d6211ad1bbf4d3bc6baea75483a136f8
mmetr14 <mmetr14@freeuni.edu.ge>
2016-11-28 18:03:41 +0400

    comment

--- Project4/src/userprog/exception.c
+++ Project4/src/userprog/exception.c
@@ -168,6 +168,8 @@ page_fault (struct intr_frame *f)
   user = (f->error_code & PF_U) != 0;
 
   void * page_addr = (void*)pg_round_down(fault_addr);
+  //will check for stack growth after implementing supplemental table managment
+
 
 
   /* To implement virtual memory, delete the rest of the function
