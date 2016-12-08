511e4bdad05c39ebb7d4da7bd1e64f61d531c11d
Michael <mmetr14@freeuni.edu.ge>
2016-12-09 02:42:32 +0400

    nearly implemented eviction

--- Project4/src/vm/frame.c
+++ Project4/src/vm/frame.c
@@ -5,6 +5,7 @@
 #include "frame.h"
 #include <list.h>
 #include "threads/synch.h"
+#include "threads/thread.h"
 
 void init_frame_table(void){
 	list_init(&frame_list);
@@ -33,15 +34,36 @@ void * eviction(uint8_t *upage,enum palloc_flags flags){
 	size_t size = hash_size(&frame_list);
 	size_t counter = 0;
 	struct frame_entry* evicted = NULL;
+	struct list_elem* temp = NULL;
 	while(true){
 		if(counter == ITERATION_NUM * size)break;
-		//TODO pick evitable one
-
+		if(temp == NULL || temp == list_end(&frame_list)){
+			temp = list_begin(&frame_list);
+		}else{
+			temp = list_next(temp);
+		}
+		struct frame_entry* curElem = (struct frame_entry * ) list_entry(temp,struct frame_entry,elem);
+		if(curElem->is_pinned)continue;
+		struct thread* curThread = (struct thread*)thread_current();
+		if(!pagedir_is_accessed(curThread->pagedir,curElem->upage)){
+			evicted = curElem;
+			break;
+		}else{
+			pagedir_set_accessed(curThread->pagedir,curElem->upage,false);
+		}
 		counter++;
 	}
 	if(evicted == NULL){
 		PANIC("NOTHING EVICTED. GET MORE RAM");
 	}
+	if(evicted->occupying_thread == NULL){
+		PANIC("EVICTED ELEMENT'S THREAD INFO MISSING");
+	}
+	//need to check evicted element's occupying thread's pagedir..thinking about how to do that
+	pagedir_clear_page(evicted->occupying_thread->pagedir,evicted->upage);
+	//next steps require swap..waiting for it
+
+
 
 }
 
