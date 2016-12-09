0e9cd04d2b9c5497f5fb5068f620be4b9264a2b1
Alexander Maghradze <amagh13@freeuni.edu.ge>
2016-12-09 18:44:11 +0400

    swap init

--- Project4/src/vm/swap.c
+++ Project4/src/vm/swap.c
@@ -1,1 +1,14 @@
 #include "swap.h"
+#include "threads/vaddr.h"
+#include "devices/block.h"
+#include <bitmap.h>
+#include "threads/synch.h"
+struct block *swapblock;
+struct bitmap *swapmap;
+struct lock swaplock;
+void swap_init (void){
+	lock_init(&swaplock);
+	swapblock = block_get_role(BLOCK_SWAP);
+	swapmap = bitmap_create(block_size(swapblock) / (PGSIZE / BLOCK_SECTOR_SIZE));
+	bitmap_set_all(swapmap,1);
+}
