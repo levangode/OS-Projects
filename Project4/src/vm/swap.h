fb0dcade6d590912ff611871cbb28161a7cc4cc9
Alexander Maghradze <amagh13@freeuni.edu.ge>
2016-12-09 18:41:27 +0400

    Swap.h

--- Project4/src/vm/swap.h
+++ Project4/src/vm/swap.h
@@ -1,27 +1,8 @@
 #ifndef _SWAP_
 #define _SWAP_
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void swap_init (void);
+int swap_out (void *swap_pg);
+void swap_in (int index, void *swap_pg);
+void swap_free (int index);
 
 #endif
