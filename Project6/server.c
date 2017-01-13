38acf8d393cfa2ba8c9afca660498404a0a2d23d
levangode <lgode14@freeuni.edu.ge>
2017-01-13 20:15:53 +0400

    initial working version
    telnet localhost 3490 will return Hello World

--- Project6/server.c
+++ Project6/server.c
@@ -11,17 +11,20 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <assert.h>
+#include <stdbool.h>
 
+#define BACKLOG 128
 
 
 int main(int argc, char *argv[]){
-	int socket_fd = -1;
+	int socket_fd, client_fd = -1;
 	int success;
+	int sin_size;
 
-	int port;
+	int port = 3490;
 	assert(port > 1024 && port <= 65535);
 
-	struct sockaddr_in my_addr;
+	struct sockaddr_in my_addr, client_addr;
 
 
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -38,13 +41,13 @@ int main(int argc, char *argv[]){
 
 	//myport and myip to be defined
 	my_addr.sin_family = AF_INET;	//host byte order
-	my_addr.sin_port = htons(MYPORT);	//short, network byte order
-	my_addr.sin_addr.s_addr = inet_addr(MYIP);
+	my_addr.sin_port = htons(port);	//short, network byte order
+	my_addr.sin_addr.s_addr = INADDR_ANY;//inet_addr(MYIP);
 	memset(&(my_addr.sin_zero), '\0', 8);
 
 	//ERRORCHEKING
 	if(my_addr.sin_addr.s_addr == -1){
-		perror("255.255.255.255 Happened :(");
+		perror( "255.255.255.255 Happened :(" );
 	}
 	////
 	
@@ -55,10 +58,21 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 	////
-	success = listen(socket_fd, 128);
+	success = listen(socket_fd, BACKLOG);
 	if(success == -1){
 		perror("Could not listen");
 		exit(1);
+	}
+
+	while(true){
+		sin_size = sizeof(struct sockaddr_in);
+		client_fd = accept(socket_fd, (struct sockaddr*)&client_addr, &sin_size);
+		if(client_fd == -1){
+			perror("Couldn't accept");
+			continue;
+		}
+		printf("server: got connection from %s\n", inet_ntoa(client_addr.sin_addr));
+		send(client_fd, "Hello, World!\n", 14, 0);
 	}
 
 
